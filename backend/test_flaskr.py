f62c3c03e7f5e1abfb2a1113760d7c8e40c249b1
Said <s.lotus69@gmail.com>
2020-04-27 09:45:44 +0200

    feat: modify a test deletion question existing in the common database

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -62,14 +62,14 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(len(data['questions']))
 
     def test_delete_question(self):
-        res = self.client().delete('/questions/54')
+        res = self.client().delete('/questions/4')
         data = json.loads(res.data)
 
         question = Question.query.filter(Question.id == 1).one_or_none()
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(data['deleted'], 54)
+        self.assertEqual(data['deleted'], 4)
         self.assertTrue(data['total_questions'])
         self.assertTrue(len(data['questions']))
         self.assertEqual(question, None)
